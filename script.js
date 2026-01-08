67d94ec39b238003367cec759a061e5a7f3611b3
mech071 <suprit2snehasis@gmail.com>
2026-01-08 12:22:21 +0530

    optimized UI

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-const btn=document.querySelector(".btn");
+const form=document.querySelector(".search");
 const input=document.querySelector(".input");
 const container=document.querySelector(".container");
+const loader = document.querySelector(".loader");
 const api="YGTNR5QNDJV2BXP2E3A876RPX";
 
-btn.addEventListener("click", async (e) => {
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const city=input.value;
-    if(city) {
-        try {
+    const city=input.value.trim();
+    if(!city) {
+        displayError("Please enter a city");
+        return;
+    }
+    loader.classList.remove("hidden");
+    container.classList.add("hidden");
+    try {
             const weatherData = await getWeather(city);
+            loader.classList.add("hidden");
             display(weatherData);
         }
         catch(error) {
+            loader.classList.add("hidden");
             console.error(error);
             displayError(error.message);
         }
-    }
-    else {
-        displayError("Please enter a city");
-    }
 })
 
 async function getWeather(city) {
     
     const url=`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=${api}&contentType=json`; 
     const res = await fetch(url);
-    if(!res.ok) throw new Error("Could not fetch weather data!");
+    if(!res.ok) throw new Error("City not found!");
     return await res.json();
 }
 
